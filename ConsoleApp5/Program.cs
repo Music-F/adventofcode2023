5fb6e7ef6232b5097dad823ed443f0b686f39c1f
MusicF <sergey.kolotiy.mg@gmail.com>
2023-12-11 00:15:10 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Xml.XPath;
+
+
+int GetPredictSum(string sequencesStr)
+{
+    int result = 0;
+
+    string[] sequences = sequencesStr.Split("\r\n");
+
+    foreach (string sequence in sequences)
+    {
+        int[] values = sequence.Split(' ').Select(int.Parse).ToArray();
+
+        for (int i = 0; i < values.Length - 1; i++)
+        {
+            
+        }
+    }
+
+    return result;
+}
---
+++ ConsoleApp5/SearchUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class SearchUtils
+    {
+        int BinarySearch(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = ((right - left) / 2) + left;
+                if (target > nums[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
